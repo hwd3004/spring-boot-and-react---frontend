9ae3456dfc74e43c6a6c45b006a3428d59235c29
liufeier <hwd3004@gmail.com>
2023-01-12 23:43:10 +0900

    initial

---
+++ src/modules/axiosInstance.ts
@@ -0,0 +1,47 @@
+import axios, { AxiosHeaders } from "axios";
+
+const BACKEND_ROOT_ENDPOINT = "http://localhost:8080/api";
+
+const axiosInstance = axios.create({
+  baseURL: BACKEND_ROOT_ENDPOINT,
+  // timeout: 1000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+interface CustomAxiosHeaders extends AxiosHeaders {
+  Authorization?: string;
+}
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    const heaers = config.headers as CustomAxiosHeaders;
+
+    if (token) {
+      heaers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.reload();
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
